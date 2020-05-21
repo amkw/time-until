8c375f5d92eb8994172fc3e55acb619d958db153
Alyssa Wei <wei.alyssa@gmail.com>
2020-05-21 15:55:55 -0700

    Rendering functional (countdown by seconds, etc.) Timer component

--- src/Timer.js
+++ src/Timer.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import getTimeDiff from "./utils";
 
 const Timer = props => {
   const [eventName, eventDate, eventTime] = props.data;
-  const { days, hours, minutes, seconds } = getTimeDiff(eventDate, eventTime);
+
+  const [days, updateDays] = useState(getTimeDiff(eventDate, eventTime).days);
+  const [hours, updateHours] = useState(
+    getTimeDiff(eventDate, eventTime).hours
+  );
+  const [minutes, updateMinutes] = useState(
+    getTimeDiff(eventDate, eventTime).minutes
+  );
+  const [seconds, updateSeconds] = useState(
+    getTimeDiff(eventDate, eventTime).seconds
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const { days, hours, minutes, seconds } = getTimeDiff(
+        eventDate,
+        eventTime
+      );
+      updateDays(days);
+      updateHours(hours);
+      updateMinutes(minutes);
+      updateSeconds(seconds);
+      // console.log(days, hours, minutes, seconds);
+      // console.log(outerSecs.current);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [eventDate, eventTime]);
 
   return (
     <div>
